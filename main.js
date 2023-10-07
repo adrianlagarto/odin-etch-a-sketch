ee8129937e6ce28fee1d109ad3a9958decc61c2c
Adrian Lagarto <adrianlagartomc@gmail.com>
2023-10-06 23:53:04 -0700

    finished

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 let color = "black";
+let click = true;
 
 document.addEventListener("DOMContentLoaded", function(){
   createBoard(16);
+
+  document.querySelector("body").addEventListener("click", function(e){
+      if(e.target.tagName != "BUTTON"){
+        click = !click;
+        let draw = document.querySelector("#draw");
+        if (click){
+          draw.innerHTML = "Now you can draw"
+        }else{
+          draw.innerHTML = "You're NOw Allowed To Draw";
+        }
+      }
+  })
 
   let btnPopUp = document.querySelector('#pop-up')
   btnPopUp.addEventListener("click", function(){
@@ -42,11 +55,14 @@ function getSizeBoard() {
 }
 
 function colorDiv(){
-  if(color === "random"){
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  }
-  else{
-    this.style.backgroundColor = 'black';
+    
+  if(click){
+    if(color === "random"){
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else{
+      this.style.backgroundColor = 'black';
+    }
   }
 }
 
